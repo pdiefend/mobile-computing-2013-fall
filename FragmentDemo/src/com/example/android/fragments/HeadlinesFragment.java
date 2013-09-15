5d992886b6872ad230325b8e6d798cd04d43aff9
prd005@bucknell.edu <prd005@bucknell.edu@b6f9ce12-30d1-de5d-5b22-14761f1f5a63>
2013-09-15 17:58:48 +0000

    Got the article fragment modified such that it shows a text box with a send button. Next is to add a text field to store the last time typed into each article. Also refactoring will follow to rename the files.

--- FragmentDemo/src/com/example/android/fragments/HeadlinesFragment.java
+++ FragmentDemo/src/com/example/android/fragments/HeadlinesFragment.java
@@ -24,57 +24,63 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class HeadlinesFragment extends ListFragment {
-    OnHeadlineSelectedListener mCallback;
+	OnHeadlineSelectedListener mCallback;
 
-    // The container Activity must implement this interface so the frag can deliver messages
-    public interface OnHeadlineSelectedListener {
-        /** Called by HeadlinesFragment when a list item is selected */
-        public void onArticleSelected(int position);
-    }
+	// The container Activity must implement this interface so the frag can
+	// deliver messages
+	public interface OnHeadlineSelectedListener {
+		/** Called by HeadlinesFragment when a list item is selected */
+		public void onArticleSelected(int position);
+	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 
-        // We need to use a different list item layout for devices older than Honeycomb
-        int layout = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ?
-                android.R.layout.simple_list_item_activated_1 : android.R.layout.simple_list_item_1;
+		// We need to use a different list item layout for devices older than
+		// Honeycomb
+		int layout = Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB ? android.R.layout.simple_list_item_activated_1
+				: android.R.layout.simple_list_item_1;
 
-        // Create an array adapter for the list view, using the Ipsum headlines array
-        setListAdapter(new ArrayAdapter<String>(getActivity(), layout, Ipsum.Headlines));
-    }
+		// Create an array adapter for the list view, using the Ipsum headlines
+		// array
+		setListAdapter(new ArrayAdapter<String>(getActivity(), layout,
+				new String[] { "Article One", "Article Two" }));
+	}
 
-    @Override
-    public void onStart() {
-        super.onStart();
+	@Override
+	public void onStart() {
+		super.onStart();
 
-        // When in two-pane layout, set the listview to highlight the selected list item
-        // (We do this during onStart because at the point the listview is available.)
-        if (getFragmentManager().findFragmentById(R.id.article_fragment) != null) {
-            getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        }
-    }
+		// When in two-pane layout, set the listview to highlight the selected
+		// list item
+		// (We do this during onStart because at the point the listview is
+		// available.)
+		if (getFragmentManager().findFragmentById(R.id.article_fragment) != null) {
+			getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+		}
+	}
 
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
+	@Override
+	public void onAttach(Activity activity) {
+		super.onAttach(activity);
 
-        // This makes sure that the container activity has implemented
-        // the callback interface. If not, it throws an exception.
-        try {
-            mCallback = (OnHeadlineSelectedListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnHeadlineSelectedListener");
-        }
-    }
+		// This makes sure that the container activity has implemented
+		// the callback interface. If not, it throws an exception.
+		try {
+			mCallback = (OnHeadlineSelectedListener) activity;
+		} catch (ClassCastException e) {
+			throw new ClassCastException(activity.toString()
+					+ " must implement OnHeadlineSelectedListener");
+		}
+	}
 
-    @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        // Notify the parent activity of selected item
-        mCallback.onArticleSelected(position);
-        
-        // Set the item as checked to be highlighted when in two-pane layout
-        getListView().setItemChecked(position, true);
-    }
+	@Override
+	public void onListItemClick(ListView l, View v, int position, long id) {
+		// Notify the parent activity of selected item
+		mCallback.onArticleSelected(position);
+
+		// Set the item as checked to be highlighted when in two-pane layout
+		getListView().setItemChecked(position, true);
+	}
 }
