ec871579b1c164c875446a6444d0a133278a5990
prd005@bucknell.edu <prd005@bucknell.edu@b6f9ce12-30d1-de5d-5b22-14761f1f5a63>
2013-11-06 21:44:37 +0000

    Added String parsing to setting elements. Figuring out some stuff that is not needed.

---
+++ TLE_Test/src/com/Plan13_Test/ImportTest.java
@@ -0,0 +1,78 @@
+package com.Plan13_Test;
+
+import java.util.Scanner;
+
+public class ImportTest {
+
+	private static final boolean DEBUG = true;
+
+	public static void main(String[] args) {
+		String tle = "ISS (ZARYA)\n1 25544U 98067A   08264.51782528 -.00002182  00000-0 -11606-4 0  2927\n2 25544  51.6416 247.4627 0006703 130.5360 325.0288 15.72125391563537";
+
+		if (DEBUG) {
+			System.out.println("Working with:");
+			System.out.println(tle + "\n");
+		}
+
+		Scanner scan = new Scanner(tle);
+		String line = scan.nextLine();
+
+		String friend = "";
+		// filter out the first Line
+		if (!line.startsWith("1")) {
+			friend = line + ""; // F'ing Java
+			line = scan.nextLine();
+		}
+		if (DEBUG) {
+			System.out.println("Line 1:");
+			System.out.println(line + "\n");
+		}
+
+		int satNum = Integer.parseInt(line.substring(2, 7)); // SatNum
+		double YE = Double.parseDouble(line.substring(18, 20)); // Epoch Year
+		double TE = Double.parseDouble(line.substring(20, 32)); // Epoch Day
+
+		String m2 = "";
+		for (int i = Integer.parseInt(line.substring(60, 61)); i > 0; i--) {
+			m2 += "0";
+		}
+		m2 = line.substring(53, 54) + "0." + m2 + line.substring(54, 59);
+		double M2 = Double.parseDouble(m2); // M2 = BSTAR drag term
+
+		// Get next Line
+		line = scan.nextLine();
+		scan.close();
+		if (DEBUG) {
+			System.out.println("Line 2:");
+			System.out.println(line + "\n");
+		}
+
+		double IN = Double.parseDouble(line.substring(8, 16)); // Inclination
+		double RA = Double.parseDouble(line.substring(17, 25)); // Right
+																// Ascension
+		double EC = Double.parseDouble("0." + line.substring(26, 33)); // Eccentricity
+		double WP = Double.parseDouble(line.substring(34, 42)); // Argument of
+																// Perigee
+		double MA = Double.parseDouble(line.substring(43, 51)); // Mean Anomaly
+		double MM = Double.parseDouble(line.substring(52, 63)); // Mean Motion
+		double RV = Double.parseDouble(line.substring(63, 68)); // Revolution
+																// Number
+		double ALON = 180; // Satellite Attitude
+
+		if (DEBUG) {
+			System.out.println("Friendly Name: " + friend);
+			System.out.println("satNum: " + satNum);
+			System.out.println("YE: " + YE);
+			System.out.println("TE: " + TE);
+			System.out.println("M2: " + M2);
+			System.out.println("IN: " + IN);
+			System.out.println("RA: " + RA);
+			System.out.println("EC: " + EC);
+			System.out.println("WP: " + WP);
+			System.out.println("MA: " + MA);
+			System.out.println("MM: " + MM);
+			System.out.println("RV: " + RV);
+			System.out.println("ALON: " + ALON);
+		}
+	}
+}
--- TLE_Test/src/com/Plan13_Test/Plan13_Test.java
+++ TLE_Test/src/com/Plan13_Test/Plan13_Test.java
@@ -6,18 +6,17 @@ public class Plan13_Test {
 
 	public static void main(String[] args) {
 		Plan13 p13 = new Plan13();
-		p13.setFrequency(435300000, 145920000);// AO-51 frequency
+		// prd005: Don't need
+		// p13.setFrequency(435300000, 145920000);// AO-51 frequency
+
 		p13.setLocation(-64.375, 45.8958, 20); // Sackville, NB
+
+		// prd005: Don't need
 		// Serial.begin(38400);
 
 		p13.setTime(2009, 10, 1, 19, 5, 0); // Oct 1, 2009 19:05:00 UTC
 		p13.setElements(2009, 232.55636497, 98.0531, 238.4104, 83652 * 1.0e-7,
-				290.6047, 68.6188, 14.406497342, -0.00000001, 27022, 180.0); // fairly
-																				// recent
-																				// keps
-																				// for
-																				// AO-51
-																				// //readElements();
+				290.6047, 68.6188, 14.406497342, -0.00000001, 27022, 180.0);
 
 		p13.initSat();
 		p13.satvec();
@@ -25,6 +24,6 @@ public class Plan13_Test {
 		p13.printdata();
 		System.out.println("\n");
 		System.out.print("Should be: ");
-		System.out.println("AZ:57.07 EL: 4.05 RX 435301728 TX 145919440");
+		System.out.println("AZ:57.07 EL: 4.05");
 	}
 }
