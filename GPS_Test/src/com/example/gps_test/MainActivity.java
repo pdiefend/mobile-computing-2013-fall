2e2c6a073efeb42600c80357c8a5ff5ff9180cbf
prd005@bucknell.edu <prd005@bucknell.edu@b6f9ce12-30d1-de5d-5b22-14761f1f5a63>
2013-11-13 01:07:08 +0000

    GPS works. Lat and Lon over cell network. Lat, Long, and Alt over GPS.

--- GPS_Test/src/com/example/gps_test/MainActivity.java
+++ GPS_Test/src/com/example/gps_test/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.gps_test;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Location;
+import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,11 +25,48 @@ public class MainActivity extends Activity {
 
 		Log.i(TAG, "Created");
 
-		GPSThread thread = new GPSThread();
+		// Acquire a reference to the system Location Manager
+		LocationManager locationManager = (LocationManager) this
+				.getSystemService(Context.LOCATION_SERVICE);
 
-		LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-		thread.receiveLocManager(locationManager);
-		thread.run();
+		// Define a listener that responds to location updates
+		LocationListener locationListener = new LocationListener() {
+			public void onLocationChanged(Location location) {
+				// Called when a new location is found by the network location
+				// provider.
+				// makeUseOfNewLocation(location);
+				Log.i(TAG, "latitude: " + location.getLatitude());
+				Log.i(TAG, "longitude: " + location.getLongitude());
+				Log.i(TAG, "altitude: " + location.getAltitude());
+			}
+
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
+			}
+
+			public void onProviderEnabled(String provider) {
+			}
+
+			public void onProviderDisabled(String provider) {
+			}
+		};
+
+		// Register the listener with the Location Manager to receive location
+		// updates
+		// String locationProvider = LocationManager.NETWORK_PROVIDER;
+		// Or, use GPS location data:
+		String locationProvider = LocationManager.GPS_PROVIDER;
+
+		
+		locationManager.requestLocationUpdates(
+				locationProvider, 0, 0, locationListener);
+
+		// GPSThread thread = new GPSThread();
+
+		// LocationManager locationManager = (LocationManager)
+		// getSystemService(Context.LOCATION_SERVICE);
+		// thread.receiveLocManager(locationManager);
+		// thread.run();
 		return true;
 	}
 
